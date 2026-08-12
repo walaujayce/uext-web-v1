1cee9442c7fd0e0a73ffa04d82bf1d24c1235cc5
Jayce <jayce@uneotech.com>
2026-08-12 10:57:54 +0800

    no message

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/Footer.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./JS/AuthContext.jsx";
 import { FloorSectionProvider } from "./JS/FloorSectionContext.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import "./i18n.js";
 
 const savedTheme = localStorage.getItem("theme");
@@ -19,12 +20,14 @@ if (savedTheme === "dark") {
 createRoot(document.getElementById("root")).render(
   <>
     <BrowserRouter>
-      <AuthProvider>
-        <FloorSectionProvider>
-          <App />
-          <Footer />
-        </FloorSectionProvider>
-      </AuthProvider>
+      <ErrorBoundary>
+        <AuthProvider>
+          <FloorSectionProvider>
+            <App />
+            <Footer />
+          </FloorSectionProvider>
+        </AuthProvider>
+      </ErrorBoundary>
     </BrowserRouter>
   </>,
 );
